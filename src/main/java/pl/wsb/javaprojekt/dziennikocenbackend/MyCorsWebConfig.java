f80a870f2925966c227baa2ddf1c37a727a4eee1
Bartosz <bartoszkt4@gmail.com>
2023-01-11 17:09:03 +0100

    fixed REST configuration

---
+++ src/main/java/pl/wsb/javaprojekt/dziennikocenbackend/MyCorsWebConfig.java
@@ -0,0 +1,20 @@
+package pl.wsb.javaprojekt.dziennikocenbackend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MyCorsWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "OPTIONS", "PUT", "DELETE")
+                .maxAge(3600);
+    }
+
+}
